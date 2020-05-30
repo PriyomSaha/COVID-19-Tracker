75f529c078a446790f33a916ef2cd115ebc94e08
Priyom Saha <priyom1499@gmail.com>
2020-05-30 11:22:07 +0530

    update

--- src/components/Df.js
+++ src/components/Df.js
@@ -13,16 +13,17 @@ function DataFetching() {
 	const [deaths, setDeath] = useState('')
 	const [recovered, setRecovered] = useState('')
 
-	const stateCode = ['','IN-AN','IN-AP', 'IN-AR', 'IN-AS', 'IN-BR', 'IN-CH','IN-CT','IN-DN', 'IN-DD', 'IN-DL', 'IN-GA', 'IN-GJ',
-						'IN-HR', 'IN-HP', 'IN-JK', 'IN-JH', 'IN-KA', 'IN-KL','IN-LA', 'IN-LD', 'IN-MP',
-						'IN-MH', 'IN-MN', 'IN-ML', 'IN-MZ', 'IN-NL', 'IN-OR', 'IN-PY',
-						'IN-PB', 'IN-RJ','IN-SK', 'IN-TN','IN-TG', 'IN-TR', 'IN-UP','IN-UT', 'IN-WB'];
+	const stateCode = ['', 'IN-AN', 'IN-AP', 'IN-AR', 'IN-AS', 'IN-BR', 'IN-CH', 'IN-CT', 'IN-DN', 'IN-DD', 'IN-DL', 'IN-GA', 'IN-GJ',
+		'IN-HR', 'IN-HP', 'IN-JK', 'IN-JH', 'IN-KA', 'IN-KL', 'IN-LA', 'IN-LD', 'IN-MP',
+		'IN-MH', 'IN-MN', 'IN-ML', 'IN-MZ', 'IN-NL', 'IN-OR', 'IN-PY',
+		'IN-PB', 'IN-RJ', 'IN-SK', 'IN-TN', 'IN-TG', 'IN-TR', 'IN-UP', 'IN-UT', 'IN-WB'];
 
-	const state = ['Select State','Andaman and Nicobar Islands','Andhra Pradesh','Arunachal Pradesh', 'Assam', 'Bihar','Chandigarh','Chhattisgarh','Dadra and Nagar Haveli', 'Daman and Diu', 'Delhi', 'Goa', 'Gujarat',
-					'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka	', 'Kerala','Ladakh','Lakshadweep', 'Madhya Pradesh',
-					'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry',
-					'Punjab', 'Rajasthan','Sikkim', 'Tamil Nadu','Telangana', 'Tripura', 'Uttar Pradesh','Uttarakhand', 'West Bengal'];
-	
+	const state = ['Select State', 'Andaman and Nicobar Islands', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh', 'Dadra and Nagar Haveli', 'Daman and Diu', 'Delhi', 'Goa', 'Gujarat',
+		'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka	', 'Kerala', 'Ladakh', 'Lakshadweep', 'Madhya Pradesh',
+		'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry',
+		'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal'];
+
+	useEffect(() => {
 		axios({
 			"method": "GET",
 			"url": 'https://api.covidindiatracker.com/total.json',
@@ -32,12 +33,12 @@ function DataFetching() {
 				setActive(res.data.active);
 				setRecovered(res.data.recovered);
 				setDeath(res.data.deaths);
-				
+
 			})
 			.catch((error) => {
 				console.log(error)
 			})
-	
+	}, [])
 	useEffect(() => {
 		axios({
 			"method": "GET",
@@ -53,14 +54,13 @@ function DataFetching() {
 
 	const handleClick = () => {
 		setIdFromButtonClick(id);
-		
+
 		posts.forEach(element => {
-			if(element.id === id)
-			{
+			if (element.id === id) {
 				setConfirmed(element.confirmed);
 				setActive(element.active);
 				setRecovered(element.recovered);
-				setDeath(element.deaths);				
+				setDeath(element.deaths);
 			}
 		});
 	}
@@ -75,12 +75,12 @@ function DataFetching() {
 						}
 					</select>
 				</div>
-				
+
 				<div className="col">
 					<button type="button" className="btn btn-primary" onClick={handleClick}>Result</button>
 				</div>
 			</div>
-			<br/>
+			<br />
 			<Data confirmed={confirmed} active={active} recovered={recovered} deaths={deaths} />
 		</div>
 	)
