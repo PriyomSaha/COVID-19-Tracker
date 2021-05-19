e39cab5e73e6b78ae52ca18e6695491b3b62715f
Priyom Saha <priyom1499@gmail.com>
2021-05-19 10:57:21 +0530

    District wise Data

--- src/components/Df.js
+++ src/components/Df.js
@@ -4,86 +4,83 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Data from './Data'
 
 function DataFetching() {
-	const [posts, setPosts] = useState([])
-	const [id, setId] = useState('')
-	const [idFromButtonClick, setIdFromButtonClick] = useState('')
+const [posts, setPosts] = useState([])
+const [distData, setDistData] = useState('')
+const [states, setStates] = useState('')
 
-	const [confirmed, setConfirmed] = useState('');
-	const [active, setActive] = useState('')
-	const [deaths, setDeath] = useState('')
-	const [recovered, setRecovered] = useState('')
+const [confirmed, setConfirmed] = useState('----');
+const [active, setActive] = useState('----')
+const [deaths, setDeath] = useState('----')
+const [recovered, setRecovered] = useState('----')
 
-	const stateCode = ['', 'IN-AN', 'IN-AP', 'IN-AR', 'IN-AS', 'IN-BR', 'IN-CH', 'IN-CT', 'IN-DN', 'IN-DD', 'IN-DL', 'IN-GA', 'IN-GJ',
-		'IN-HR', 'IN-HP', 'IN-JK', 'IN-JH', 'IN-KA', 'IN-KL', 'IN-LA', 'IN-LD', 'IN-MP',
-		'IN-MH', 'IN-MN', 'IN-ML', 'IN-MZ', 'IN-NL', 'IN-OR', 'IN-PY',
-		'IN-PB', 'IN-RJ', 'IN-SK', 'IN-TN', 'IN-TG', 'IN-TR', 'IN-UP', 'IN-UT', 'IN-WB'];
+const [dist, setDist] = useState(['Select District'])
 
-	const state = ['Select State', 'Andaman and Nicobar Islands', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh', 'Dadra and Nagar Haveli', 'Daman and Diu', 'Delhi', 'Goa', 'Gujarat',
-		'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka	', 'Kerala', 'Ladakh', 'Lakshadweep', 'Madhya Pradesh',
-		'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry',
-		'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal'];
+const stateCode = ['', 'IN-AN', 'IN-AP', 'IN-AR', 'IN-AS', 'IN-BR', 'IN-CH', 'IN-CT', 'IN-DN', 'IN-DD', 'IN-DL', 'IN-GA', 'IN-GJ',
+'IN-HR', 'IN-HP', 'IN-JK', 'IN-JH', 'IN-KA', 'IN-KL', 'IN-LA', 'IN-LD', 'IN-MP',
+'IN-MH', 'IN-MN', 'IN-ML', 'IN-MZ', 'IN-NL', 'IN-OR', 'IN-PY',
+'IN-PB', 'IN-RJ', 'IN-SK', 'IN-TN', 'IN-TG', 'IN-TR', 'IN-UP', 'IN-UT', 'IN-WB'];
 
-	useEffect(() => {
-		axios({
-			"method": "GET",
-			"url": 'https://api.covidindiatracker.com/total.json',
-		})
-			.then((res) => {
-				setConfirmed(res.data.confirmed);
-				setActive(res.data.active);
-				setRecovered(res.data.recovered);
-				setDeath(res.data.deaths);
+const state = ['Select State', 'Andaman and Nicobar Islands', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh',
+'Chhattisgarh', 'Dadra and Nagar Haveli', 'Daman and Diu', 'Delhi', 'Goa', 'Gujarat',
+'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka	', 'Kerala', 'Ladakh', 'Lakshadweep', 'Madhya Pradesh',
+'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry',
+'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal'];
 
-			})
-			.catch((error) => {
-				console.log(error)
-			})
-	}, [])
-	useEffect(() => {
-		axios({
-			"method": "GET",
-			"url": 'https://api.covidindiatracker.com/state_data.json',
-		})
-			.then((res) => {
-				setPosts(res.data)
-			})
-			.catch((error) => {
-				console.log(error)
-			})
-	}, [idFromButtonClick])
+useEffect(() => {
+axios({
+	"method": "GET",
+	"url": 'https://api.covid19india.org/state_district_wise.json',
+})
+	.then((res) => {
+		setPosts(res.data)
 
-	const handleClick = () => {
-		setIdFromButtonClick(id);
+	})
+	.catch((error) => {
+		console.log(error)
+	})
+},[]);
 
-		posts.forEach(element => {
-			if (element.id === id) {
-				setConfirmed(element.confirmed);
-				setActive(element.active);
-				setRecovered(element.recovered);
-				setDeath(element.deaths);
-			}
-		});
-	}
-	return (
-		<div>
-			<div className="row">
-				<div className="col">
-					<select className="select" onChange={e => setId(e.target.value)}>
-						{
-							stateCode.map((x, index) =>
-								<option key={x} value={x}>{state[index]}</option>)
-						}
-					</select>
-				</div>
+const handleResult = () => {
+console.log(posts[states].districtData[distData]);
+setConfirmed(posts[states].districtData[distData].confirmed)
+setActive(posts[states].districtData[distData].active)
+setDeath(posts[states].districtData[distData].deceased)
+setRecovered(posts[states].districtData[distData].recovered)
+}
 
-				<div className="col">
-					<button type="button" className="btn btn-primary" onClick={handleClick}>Result</button>
-				</div>
-			</div>
-			<br />
-			<Data confirmed={confirmed} active={active} recovered={recovered} deaths={deaths} />
+const handleStatesChange = (e) =>{
+var keys =['Select District']
+setStates(e.target.value)
+for(var k in posts[e.target.value].districtData) keys.push(k);
+setDist(keys)}
+
+return (
+<div>
+	<div className="row">
+		<div className="col">
+			<select className="select" onChange={handleStatesChange}>
+				{
+					state.map((x, index) =>
+						<option key={x} value={x}>{state[index]}</option>)
+				}
+			</select>
+
+			<select className="select state" onChange = {e => (setDistData(e.target.value))}>
+				{
+					dist.map((value) =>
+						<option>{value}</option>)
+				}
+			</select>
 		</div>
-	)
+
+		<div className="col-4">
+			<button type="button" className="btn btn-primary" onClick={handleResult}>Result</button>
+		</div>
+	</div>
+	<br />
+	<Data confirmed={confirmed} active={active} recovered={recovered} deaths={deaths} />
+</div>
+)
 }
 
 export default DataFetching
